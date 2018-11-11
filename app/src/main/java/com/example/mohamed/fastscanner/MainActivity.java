513ab8982fa6e86d95d9c67fd9562519d221a695
MohamedHashim <sawmido2000@gmail.com>
2018-11-12 01:57:23 +0200

    Implement file provider to support the higher Android versions

--- app/src/main/java/com/example/mohamed/fastscanner/MainActivity.java
+++ app/src/main/java/com/example/mohamed/fastscanner/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.mohamed.fastscanner;
 
+import android.Manifest;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.nfc.NfcAdapter;
 import android.nfc.tech.IsoDep;
@@ -13,8 +15,12 @@ import android.nfc.tech.NfcA;
 import android.nfc.tech.NfcB;
 import android.nfc.tech.NfcF;
 import android.nfc.tech.NfcV;
+import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -27,12 +33,12 @@ import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_PERMISSIONS_CODE_WRITE_EXTERNAL_STORAGE = 100;
     TextView text;
     String id;
     Button save;
     FileWriter writer;
-    File root = Environment.getExternalStorageDirectory();
-    File gpxfile = new File(root, "UIDs.csv");
+    File gpxfile;
     private NfcAdapter mNfcAdapter;
     private final String[][] techList = new String[][]{
             new String[]{
@@ -51,8 +57,17 @@ public class MainActivity extends AppCompatActivity {
 //        themeColor = R.color.intro1_bg;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_PERMISSIONS_CODE_WRITE_EXTERNAL_STORAGE);
+            } else {
+                Toast.makeText(this, "Please enable storage permission", Toast.LENGTH_LONG).show();
+                openSettings();
+            }
+        }
         text = (TextView) findViewById(R.id.text);
-        save= (Button) findViewById(R.id.save);
+        save = (Button) findViewById(R.id.save);
         mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
         if (mNfcAdapter == null) {
             // Stop here, we definitely need NFC
@@ -65,12 +80,13 @@ public class MainActivity extends AppCompatActivity {
         } else {
             text.setText("Scan NFC Card");
         }
+        gpxfile = new File(getFilesDir(), "UIDs.csv");
 
         try {
 
             writer = new FileWriter(gpxfile);
 
-            writeCsvHeader("First Param","Second Param","Third Param");
+            writeCsvHeader("First Param", "Second Param", "Third Param");
 //            writeCsvData(0.31f,5.2f,7.0f);
 //            writeCsvData(0.31f,5.2f,7.1f);
 //            writeCsvData(0.31f,5.2f,7.2f);
@@ -82,7 +98,7 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
-        if(!save.isEnabled())
+        if (!save.isEnabled())
             save.setBackgroundColor(0x00C089);
 
         save.setOnClickListener(new View.OnClickListener() {
@@ -93,7 +109,7 @@ public class MainActivity extends AppCompatActivity {
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-//                try {
+//                   try {
 //                    writer.close();
 //                } catch (IOException e) {
 //                    e.printStackTrace();
@@ -102,15 +118,15 @@ public class MainActivity extends AppCompatActivity {
 
                 Intent intentShareFile = new Intent(Intent.ACTION_SEND);
 
-                if(gpxfile.exists()) {
+                if (gpxfile.exists()) {
+                    Uri uri = FileProvider.getUriForFile(getApplicationContext(), "com.example.mohamed.fastscanner", gpxfile);
                     intentShareFile.setType("text/comma-separated-values");
-//                    intentShareFile.setData(Uri.fromFile(gpxfile));
-                    intentShareFile.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(gpxfile));
-
-                    intentShareFile.putExtra(Intent.EXTRA_SUBJECT,
-                            "Sharing File...");
+                    intentShareFile.putExtra(Intent.EXTRA_STREAM, uri);
+                    intentShareFile.putExtra(Intent.EXTRA_SUBJECT, "Sharing File...");
                     intentShareFile.putExtra(Intent.EXTRA_TEXT, "Sharing UIDs...");
-
+//                    intentShareFile.setData(uri);
+//                    intentShareFile.setData(uri);
+//                    intentShareFile.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
                     startActivity(Intent.createChooser(intentShareFile, "Share File"));
                 }
             }
@@ -177,6 +193,26 @@ public class MainActivity extends AppCompatActivity {
         finish();
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_PERMISSIONS_CODE_WRITE_EXTERNAL_STORAGE: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Thank you!", Toast.LENGTH_LONG).show();
+                } else {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_PERMISSIONS_CODE_WRITE_EXTERNAL_STORAGE);
+                    } else {
+                        Toast.makeText(this, "Please enable storage permission", Toast.LENGTH_LONG).show();
+                        openSettings();
+                    }
+                }
+            }
+
+        }
+    }
+
     private String ByteArrayToHexString(byte[] inarray) {
         int i, j, in;
         String[] hex = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"};
@@ -190,5 +226,13 @@ public class MainActivity extends AppCompatActivity {
             out += hex[i];
         }
         return out;
+    }
+
+    private void openSettings() {
+        Intent intent = new Intent();
+        intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+        Uri uri = Uri.fromParts("com.example.mohamed.fastscanner", this.getPackageName(), null);
+        intent.setData(uri);
+        startActivity(intent);
     }
 }
